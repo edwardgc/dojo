77c09c14355af25c479e8221ffb8f54c990e88d2
edwardgc <edwardgc@gmail.com>
2020-07-22 11:01:12 +0800

    init ferraris

---
+++ ferraris/two-power/Include/Main.hpp
@@ -0,0 +1,17 @@
+/*
+ * Main.hpp
+ *
+ *  Created on: 2017Äê8ÔÂ18ÈÕ
+ *      Author: cguo005
+ */
+
+#pragma once
+
+class Main
+{
+public:
+    int getInt();
+};
+
+
+
---
+++ ferraris/two-power/Source/Main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 2017Äê8ÔÂ18ÈÕ
+ *      Author: cguo005
+ */
+
+#include "Main.hpp"
+
+int Main::getInt()
+{
+    return 1;
+}
+
